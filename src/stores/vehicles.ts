7eec99a840afe8c5903ff4704ba08b84241f8ed8
CIKADA <adel.garipovazn@mail.ru>
2023-02-22 00:02:07 +0300

    Add Price, Year filters

--- src/stores/vehicles.ts
+++ src/stores/vehicles.ts
@@ -12,12 +12,21 @@ export interface IVehicle {
   longitude: number,
 }
 
+export interface IfilterValues {
+  minPrice: number,
+  maxPrice: number,
+}
+
 export const useVehiclesStore = defineStore({
   id: 'vehicles',
   state: () => (
     {
       vehicles: [] as IVehicle[],
       currentVehicle: {} as IVehicle,
+      vehicleYears: [] as number[],
+      minPrice: null,
+      maxPrice: null,
+      chosenYear: null,
     }
   ),
   actions: {
@@ -26,7 +35,9 @@ export const useVehiclesStore = defineStore({
         const response = await axios.get('https://test.tspb.su/test-task/vehicles');
         const data = await response.data;
         this.vehicles = data;
-        console.log('data: ', data);
+        console.log('fetchVehicles: ', data);
+        this.setVehicleYears();
+        this.setMinMaxPrice();
       } catch (error) {
         console.log('error: ', error);
       }
@@ -60,10 +71,59 @@ export const useVehiclesStore = defineStore({
         console.log('error: ', error);
       }
     },
+    async filterVehicles(filterValues: IfilterValues) {
+      const response = await this.fetchVehicles();
+
+      let minFilterPrice;
+      if (filterValues.minPrice === '' || filterValues.minPrice == null) {
+        minFilterPrice = 1;
+      } else {
+        minFilterPrice = filterValues.minPrice;
+      }
+
+      let maxFilterPrice;
+      if (filterValues.maxPrice === '' || filterValues.maxPrice == null) {
+        maxFilterPrice = this.maxPrice;
+      } else {
+        maxFilterPrice = filterValues.maxPrice;
+      }
+
+      console.log('minFilterPrice: ', minFilterPrice);
+      console.log('maxFilterPrice: ', maxFilterPrice);
+      let filteredVihicles = Object.values(this.vehicles).filter((item) => item.price >= minFilterPrice && item.price <= maxFilterPrice);
+      if (this.chosenYear != null) {
+        filteredVihicles = filteredVihicles.filter((item) => item.year == this.chosenYear);
+      }
+      
+      console.log('filteredVihicles: ', filteredVihicles);
+      this.vehicles = filteredVihicles;
+    },
+    async filterByYear (year: number) {
+      const response = await this.fetchVehicles();
+      const filteredByYear = Object.values(this.vehicles).filter((item) => item.year == year);
+      this.vehicles = filteredByYear;
+    },
+    setVehicleYears() {
+      const uniqueVehicleYears = Object.values(this.vehicles).map((item) => item.year).filter((x, i, a) => a.indexOf(x) === i);
+      this.vehicleYears = uniqueVehicleYears;
+    },
+    setMinMaxPrice() {
+      this.minPrice = Math.min(...Object.values(this.vehicles).map((item) => item.price));
+      this.maxPrice = Math.max(...Object.values(this.vehicles).map((item) => item.price));
+    },
+    setChosenYear(year: number) {
+      this.chosenYear = year;
+    },
   },
   getters: {
-    itemsCount() {
-      return this.vehicles.length;
+    getVehicleYears() {
+      return this.vehicleYears;
+    },
+    getMinPrice() {
+      return this.minPrice;
+    },    
+    getMaxPrice() {
+      return this.maxPrice;
     }
   },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,47 @@
 <template>
   <main class="vehicles py-4">
     <h1 class="vehicles__title text-5xl mb-5">Vehicles</h1>
-    <p class="vehicles__info text-lg mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae excepturi nisi doloremque animi eius assumenda doloribus laudantium! Laboriosam, itaque corporis.</p>
+    <p class="vehicles__info text-lg mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae excepturi nisi doloremque animi eius assumenda doloribus laudantium! Laboriosam, itaque corporis.</p>
+    <div class="vehicles__filters flex justify-end items-center gap-5 mb-5">
+      
+      <select class="block appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
+        v-model="vehicleYear"
+      >
+        <option 
+          v-for="year in vehiclesStore.getVehicleYears" 
+          :key="year"
+        >{{ year }}</option>
+      </select>
+
+      <div class="mb-5">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="minPrice">
+          Min price
+        </label>
+        <input
+          class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="minPrice"
+          type="number"
+          v-model="filterPrice.minPrice"
+          placeholder="Min price"
+          required
+        />
+      </div>
+
+      <div class="mb-5">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="maxPrice">
+          Max Price
+        </label>
+        <input
+          class="inline-block shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="maxPrice"
+          type="number"
+          v-model="filterPrice.maxPrice"
+          placeholder="Max price"
+          required
+        />
+      </div>
+
+    </div>
     <div class="vehicles__list flex flex-wrap justify-between items-center">
       <div
         class="vehicles__item w-full sm:w-[48%]" 
@@ -16,10 +56,39 @@
 <script setup lang="ts">
 import AppCard from '@/components/AppCard.vue'
 import { useVehiclesStore } from '@/stores/vehicles';
+import { ref, reactive, watch } from 'vue';
+
+interface IfilterValues {
+  minPrice: number | null,
+  maxPrice: number | null,
+}
 
 const vehiclesStore = useVehiclesStore();
 
 vehiclesStore.fetchVehicles();
+
+const vehicleYear = ref();
+
+const filterPrice = reactive({
+  minPrice: null,
+  maxPrice: null,
+} as IfilterValues);
+
+watch(filterPrice, (filterValues) => {
+  if (filterPrice.minPrice < 0) {
+    filterPrice.minPrice = vehiclesStore.minPrice;
+  }
+  if (filterPrice.maxPrice > vehiclesStore.maxPrice || filterPrice.maxPrice < 0) {
+    filterPrice.maxPrice = vehiclesStore.maxPrice;
+  }
+  vehiclesStore.filterVehicles(filterValues);
+});
+
+watch(vehicleYear, (newValue) => {
+  vehiclesStore.setChosenYear(newValue)
+  vehiclesStore.filterByYear(newValue);
+});
+
 
 </script>
 
