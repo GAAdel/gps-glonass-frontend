90e4ff0744b6b62d0497423cbe19594359a1da9d
CIKADA <adel.garipovazn@mail.ru>
2023-02-21 19:42:50 +0300

    Change App component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
-    <RouterView />
+  <div class="min-h-screen bg-white font-Roboto">
+
+    <TheHeader />
+
+    <AppContainer>
+      <RouterView />
+    </AppContainer>
+
+    <TheFooter />
+    
   </div>
 </template>
 
 <script setup lang="ts">
-
+import TheHeader from '@/components/TheHeader.vue'
+import AppContainer from '@/components/AppContainer.vue';
+import TheFooter from '@/components/TheFooter.vue'
 import { RouterView } from 'vue-router';
-
 </script>
-
-<style scoped>
-
-</style>
