1e8b73cb2970710c5d84bf7d7be6d1f04a2763ae
CIKADA <adel.garipovazn@mail.ru>
2023-02-21 19:42:25 +0300

    Add Footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer bottom-0 shadow-lg flex justify-center text-lg font-medium bg-green-400 p-6">
+    <nav class="footer__nav flex justify-center">
+      <ul class="footer__list flex justify-between gap-6">
+        <li class="footer__item">Main</li>
+        <li class="footer__item">About Us</li>
+        <li class="footer__item">Contacts</li>
+      </ul>
+    </nav>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<style scoped></style>
