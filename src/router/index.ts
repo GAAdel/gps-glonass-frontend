fce097185cf3dc34aef2b97b742f177af574662d
CIKADA <adel.garipovazn@mail.ru>
2023-02-21 19:41:58 +0300

    Add Vehicle route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import VehicleView from '@/views/VehicleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,15 +10,15 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ]
+    {
+      path: '/vehicle/:id',
+      name: 'vehicle',
+      component: () => import('../views/VehicleView.vue')
+    }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
 })
 
 export default router
